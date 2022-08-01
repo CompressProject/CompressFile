e5c6aeba7d0fd730334c4b91b05d5ef2e418f28b
TamarWolpa <tamark6760@gmail.com>
2022-08-01 12:07:41 +0300

    commit common functions file

---
+++ CompressFile/Common_Functions.h
@@ -0,0 +1,13 @@
+#ifndef __CommonFunction_H
+#define __CommonFunctions_H
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char* convertIntToChar(int num);
+char* append(char*, char);
+int stringLength(char*);
+int intLength(int);
+
+#endif // !__CommonFunction_H
+
