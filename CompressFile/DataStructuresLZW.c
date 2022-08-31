38c1a3e73d5486cf33b2e06a0e019cd5fff2f3a1
hadasa-huminer <h0504112782@gmail.com>
2022-08-31 10:15:44 +0300

    changes in DataStructuresLZW.c file.

--- CompressFile/DataStructuresLZW.c
+++ CompressFile/DataStructuresLZW.c
@@ -4,7 +4,7 @@ void init(CodeTable* CodeTable)
 	CodeTable->size = 256;
 	for (int i = 0; i < 256; i++)
 	{
-		char str[10] = { (char)i, "\0" };
+		char str[10] = { (char)i, '\0' };
 		CodeTable->CodeArray[i] = _strdup(str);
 	}
 	for (int i = 256; i < SIZE_TABLE; i++)
@@ -30,9 +30,36 @@ int find(CodeTable* CodeTable, char* value) {
 	return -1;
 }
 void printTable(CodeTable* CodeTable) {
-	printf("\n\n--------------------------------\n");
-	for (int i = 0; i < SIZE_TABLE; i++) {
-		printf("%4x  %8s\n", i, CodeTable->CodeArray[i]);
+#ifdef PRINT_DECOMPRESS
+	fopen_s(&decompressPrintFile, "decompressPrintFile.txt", "a+");
+	if (!decompressPrintFile)
+	{
+		printf("unable to open the decompressPrintFile file.");
+		exit(1);
 	}
-	printf("\n--------------------------------\n");
+	printf("--------------------------------\n");
+	fprintf(decompressPrintFile, "--------------------------------\n");
+	for (int i = 0; i < SIZE_TABLE; i++) {
+		printf("%4d  %8s\n", i, CodeTable->CodeArray[i]);
+		fprintf(decompressPrintFile, "%4d  %8s\n", i, CodeTable->CodeArray[i]);
+	}
+	printf("--------------------------------\n");
+	fprintf(decompressPrintFile, "--------------------------------\n");
+#endif // PRINT_DECOMPRESS
+#ifdef PRINT_COMPRESS 
+	fopen_s(&compressPrintFile, "compressPrintFile.txt", "a+");
+	if (!compressPrintFile)
+	{
+		printf("unable to open the compressPrintFile file.");
+		exit(1);
+	}
+	printf("--------------------------------\n");
+	fprintf(compressPrintFile,"--------------------------------\n");
+	for (int i = 0; i < SIZE_TABLE; i++) {
+		printf("%4d  %8s\n", i, CodeTable->CodeArray[i]);
+		fprintf(compressPrintFile,"%4d  %8s\n", i, CodeTable->CodeArray[i]);
+	}
+	printf("--------------------------------\n");
+	fprintf(compressPrintFile, "--------------------------------\n");
+#endif // PRINT_COMPRESS
 }
