27e3e5014e6078460accc615a055606bbc1b46a6
hadasa-huminer <h0504112782@gmail.com>
2022-09-04 15:12:27 +0300

    add DataStructuresHuffman files.

---
+++ CompressFile/DataStructuresHuffman.c
@@ -0,0 +1,107 @@
+#include "DataStructuresHuffman.h"
+// A utility function allocate a new
+// min heap node with given character
+// and frequency of the character
+MinHeapNode* newNode(int data, unsigned freq)
+{
+	MinHeapNode* temp = (MinHeapNode*)malloc(sizeof(MinHeapNode));
+	temp->left = temp->right = NULL;
+	temp->data = data;
+	temp->freq = freq;
+	return temp;
+}
+// A utility function to create
+// a min heap of given capacity
+MinHeap* createMinHeap(unsigned capacity)
+{
+	MinHeap* minHeap = (MinHeap*)malloc(sizeof(MinHeap));
+	// current size is 0
+	minHeap->size = 0;
+	minHeap->capacity = capacity;
+	minHeap->array = (MinHeapNode**)malloc(minHeap->capacity * sizeof(MinHeapNode*));
+	return minHeap;
+}
+// A utility function to
+// swap two min heap nodes
+void swapMinHeapNode(MinHeapNode** a, MinHeapNode** b)
+{
+	MinHeapNode* t = *a;
+	*a = *b;
+	*b = t;
+}
+// The standard minHeapify function.
+void minHeapify(MinHeap* minHeap, int index)
+{
+	int smallest = index;
+	int left = 2 * index + 1;
+	int right = 2 * index + 2;
+	if (left < minHeap->size && minHeap->array[left]->freq < minHeap->array[smallest]->freq)
+		smallest = left;
+	if (right < minHeap->size && minHeap->array[right]->freq < minHeap->array[smallest]->freq)
+		smallest = right;
+	if (smallest != index)
+	{
+		swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[index]);
+		minHeapify(minHeap, smallest);
+	}
+}
+// A utility function to check
+// if size of heap is 1 or not
+int isSizeOne(MinHeap* minHeap)
+{
+	return (minHeap->size == 1);
+}
+// A standard function to extract
+// minimum value node from heap
+MinHeapNode* extractMin(MinHeap* minHeap)
+{
+	MinHeapNode* temp = minHeap->array[0];
+	minHeap->array[0] = minHeap->array[minHeap->size - 1];
+	--minHeap->size;
+	minHeapify(minHeap, 0);
+	return temp;
+}
+// A utility function to insert
+// a new node to Min Heap
+void insertMinHeap(MinHeap* minHeap, MinHeapNode* minHeapNode)
+{
+	++minHeap->size;
+	int i = minHeap->size - 1;
+	while (i && minHeapNode->freq < minHeap->array[(i - 1) / 2]->freq)
+	{
+		minHeap->array[i] = minHeap->array[(i - 1) / 2];
+		i = (i - 1) / 2;
+	}
+	minHeap->array[i] = minHeapNode;
+}
+// A standard function to build min heap
+void buildMinHeap(struct MinHeap* minHeap)
+{
+	int n = minHeap->size - 1;
+	int i;
+	for (i = (n - 1) / 2; i >= 0; --i)
+		minHeapify(minHeap, i);
+}
+// Utility function to check if this node is leaf
+int isLeaf(MinHeapNode* root)
+{
+	return !(root->left) && !(root->right);
+}
+// Creates a min heap of capacity
+// equal to size and inserts all character of
+// data[] in min heap. Initially size of
+// min heap is equal to capacity
+MinHeap* createAndBuildMinHeap(int data[], int freq[], int size)
+{
+	MinHeap* minHeap = createMinHeap(size);
+	//int data;
+	for (int i = 0; i < size; ++i)
+	{
+		//data = fgetc(intermediateFile);
+		minHeap->array[i] = newNode(data[i], freq[i]);
+	}
+	minHeap->size = size;
+	buildMinHeap(minHeap);
+	return minHeap;
+}
+
---
+++ CompressFile/DataStructuresHuffman.h
@@ -0,0 +1,46 @@
+#pragma once
+//typedef struct TreeNode {
+//	char data;
+//	struct TreeNode* left;
+//	struct TreeNode* right;
+//}TreeNode;
+//typedef struct NodeHeap {
+//	int frequency;
+//	struct NodeHeap* left;
+//	struct NodeHeap* right;
+//	struct NodeTree* tree;
+//}NodeHeap;
+#include "CommonFunction.h"
+#define MAX_TREE_HT 100
+// A Huffman tree node
+typedef struct MinHeapNode {
+	// One of the input characters
+	int data;
+	// Frequency of the character
+	unsigned freq;
+	// Left and right child of this node
+	struct MinHeapNode* left, * right;
+}MinHeapNode;
+
+// A Min Heap: Collection of
+// min-heap (or Huffman tree) nodes
+typedef struct MinHeap {
+	// Current size of min heap
+	unsigned size;
+	// capacity of min heap
+	unsigned capacity;
+	// Array of minheap node pointers
+	MinHeapNode** array;
+}MinHeap;
+MinHeapNode* newNode(int data, unsigned freq);
+MinHeap* createMinHeap(unsigned capacity);
+void swapMinHeapNode(MinHeapNode** a, MinHeapNode** b);
+void minHeapify(MinHeap* minHeap, int index);
+int isSizeOne(MinHeap* minHeap);
+MinHeapNode* extractMin(MinHeap* minHeap);
+void insertMinHeap(MinHeap* minHeap, MinHeapNode* minHeapNode);
+void buildMinHeap(struct MinHeap* minHeap);
+int isLeaf(MinHeapNode* root);
+MinHeap* createAndBuildMinHeap(int data[], int freq[], int size);
+
+
